dd037ac5c55f6befbbf0eebb64c31c8e76212d06
hsyoon <bagzein@gmail.com>
2022-03-28 02:17:47 +0900

    - Specifications (명세) 사용 - Query By Example 사용

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -3,9 +3,7 @@ package study.datajpa.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.*;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
@@ -250,5 +248,34 @@ class MemberRepositoryTest {
   @Test
   public void callCustom(){
     List<Member> result = memberRepository.findMemberCustom();
+  }
+
+  // --- 실무에서 쓰기 애매. left join이 되지 않음. ---
+  @Test
+  public void queryByExample() {
+    Team teamA = new Team("teamA");
+    em.persist(teamA);
+
+    Member m1 = new Member("m1", 0, teamA);
+    Member m2 = new Member("m2", 0, teamA);
+    em.persist(m1);
+    em.persist(m2);
+
+    em.flush();
+    em.clear();
+
+    // Probe
+    Member member = new Member("m1");
+    Team team = new Team("teamA");
+    member.setTeam(team);
+
+    ExampleMatcher matcher = ExampleMatcher.matching()
+      .withIgnorePaths("age");   // age의 경우 primitive 타입(int)이기 때문에 무시한다.
+
+    Example<Member> example = Example.of(member, matcher);
+
+    List<Member> result = memberRepository.findAll(example);
+
+    assertThat(result.get(0).getUsername()).isEqualTo("m1");
   }
 }
